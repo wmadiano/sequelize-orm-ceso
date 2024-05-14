5d54ade7af9a9ea65d7ecedc5acef7e714e17b58
wmadiano <wmadiano@gmail.com>
2024-05-14 21:28:47 +0800

    updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { sq } = require("./config/db");
 const models = require('./model/dbModel');
 
 // create database tables
+// it will only be executed if there are changes on the table for example new columns. otherswise no changes will be made
 async function syncDatabase() {
     try {
       await sq.sync({ alter: true });
